8eaaadd7f7a60c027e7ffe40c6d305dd162bb141
trevisaq <127609846+trevisaq@users.noreply.github.com>
2024-02-08 17:13:33 -0300

    Add files via upload

---
+++ pain.js
@@ -0,0 +1,18 @@
+var entrar = document.getElementById('entrar');
+var respostareal = 'cachoeira';
+var sleepSetTimeout_ctrl;
+
+function sleep(ms) {
+    clearInterval(sleepSetTimeout_ctrl);
+    return new Promise(resolve => sleepSetTimeout_ctrl = setTimeout(resolve, ms));
+}
+
+entrar.addEventListener('click', () => {
+    let resposta = prompt('');
+
+    if (resposta === respostareal){
+        alert('...Parabéns, você mandou bem!')
+        sleep(10000)
+        window.location.href = "Desafios/desafio1.html"
+    }
+});
