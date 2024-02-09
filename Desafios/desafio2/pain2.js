2857346d4dcbf3f3f43e3435892174a28cf0f1da
trevisaq <souzagui2t@gmail.com>
2024-02-09 16:07:50 -0300

    Remoção dos css individuais

--- Desafios/desafio2/pain2.js
+++ Desafios/desafio2/pain2.js
@@ -1,18 +1,14 @@
 var entrar = document.getElementById('entrar');
-var respostareal = '';
+var respostareal = 'raios';
 var sleepSetTimeout_ctrl;
 
-function sleep(ms) {
-    clearInterval(sleepSetTimeout_ctrl);
-    return new Promise(resolve => sleepSetTimeout_ctrl = setTimeout(resolve, ms));
-}
+
 
 entrar.addEventListener('click', () => {
     let resposta = prompt('');
 
     if (resposta === respostareal){
         alert('...Parabéns, você mandou bem!')
-        sleep(10000)
-        window.location.href = "../desafio2/desafio2.html"
+        window.location.href = "../desafio3/desafio3.html"
     }
 });
