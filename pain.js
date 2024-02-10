39f169c313371fe50ad8dcd81e9e6f022374f8ff
trevisaq <souzagui2t@gmail.com>
2024-02-09 22:43:18 -0300

    u

--- pain.js
+++ pain.js
@@ -1,13 +1,14 @@
 var entrar = document.getElementById('entrar');
-var respostareal = 'pierre';
+var respostareal = 'cachoeira';
 var sleepSetTimeout_ctrl;
 
 
 
 entrar.addEventListener('click', () => {
     let resposta = prompt('');
+
     if (resposta === respostareal){
         alert('...Parabéns, você mandou bem!')
-        window.location.href = "../desafio5/desafio5.html"
+        window.location.href = "../Desafios/desafio1/desafio1.html"
     }
 });
