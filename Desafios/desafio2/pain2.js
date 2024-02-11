5e1a982a0a9060f25f942875d22ec1b3aecea371
trevisaq <souzagui2t@gmail.com>
2024-02-11 02:08:45 -0300

    inversão de levels

--- Desafios/desafio2/pain2.js
+++ Desafios/desafio2/pain2.js
@@ -1,5 +1,5 @@
 var entrar = document.getElementById('entrar');
-var respostareal = 'wyatt';
+var respostareal = 'hachiko';
 var sleepSetTimeout_ctrl;
 
 
--- Desafios/desafio3/pain3.js
+++ Desafios/desafio3/pain3.js
@@ -1,5 +1,5 @@
 var entrar = document.getElementById('entrar');
-var respostareal = 'hachiko';
+var respostareal = 'wyatt';
 var sleepSetTimeout_ctrl;
 
 
