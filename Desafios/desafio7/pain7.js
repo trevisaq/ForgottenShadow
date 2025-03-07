b149ebc2201c66a7f4aacc02f7b561aa12a82913
trevisaq <souzagui2t>
2025-03-07 08:26:08 -0300

    new patch XD

--- Desafios/desafio7/pain7.js
+++ Desafios/desafio7/pain7.js
@@ -4,11 +4,11 @@ var sleepSetTimeout_ctrl;
 
 entrar.addEventListener('click', () => {
     let resposta = prompt('');
-
-    if (resposta === respostareal){
-        alert('Inacreditável :D')
-        window.location.href = "../Final/index.html"
-    } else{
-        alert('Quase lá... só que não')
+    
+    if (resposta === respostareal) {
+        alert('Inacreditável :D');
+        window.location.href = "../Final/index.html";
+    } else {
+        alert('Quase lá... só que não');
     }
 });
